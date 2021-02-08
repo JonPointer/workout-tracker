270a89571ab610e4ab80f7ec5d418c22b01d6f24
Jon Pointer <jonpointer@msn.com>
2021-02-07 23:18:53 -0700

    Completed all routes

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,38 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-// Another way to do this?
-// Child schema method:
-// const exercisesSchema = new Schema({
-//     type: {
-//         type: String,
-//         trim: true
-//     },
-//     name: {
-//         type: String,
-//         type: true
-//     },
-//     duration: {
-//         type: Number
-//     },
-//     weight: {
-//         type: Number
-//     },
-//     reps: {
-//         type: Number
-//     },
-//     sets: {
-//         type: Number
-//     },
-// });
-
 const workoutSchema = new Schema({
-    // day: {
-    //     type: Date,
-    // },
-    // If using the child scheme method
-    // exercises: exercisesSchema
-    // exercises: [{ type: String, name: String, duration: Number, weight: Number, reps: Number, sets: Number}]
 
     day: {
         type: Date,
@@ -49,6 +18,9 @@ const workoutSchema = new Schema({
                 type: String,
                 trim: true
             },
+            distance: {
+                type: Number
+            },
             duration: {
                 type: Number
             },
@@ -61,10 +33,10 @@ const workoutSchema = new Schema({
             sets: {
                 type: Number
             },
-        }]
+        },
+        { _id : false }
+    ]
 });
-
-
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,143 +1,12 @@
 const Workout = require("../models/workout.js");
 
-// Initial seed of database - used once to fill database and then commented out 
-
-// let workoutSeed = [
-//     {
-//       day: new Date().setDate(new Date().getDate()-10),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Bicep Curl",
-//           duration: 20,
-//           weight: 100,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date().setDate(new Date().getDate()-9),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Lateral Pull",
-//           duration: 20,
-//           weight: 300,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date().setDate(new Date().getDate()-8),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Push Press",
-//           duration: 25,
-//           weight: 185,
-//           reps: 8,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date().setDate(new Date().getDate()-7),
-//       exercises: [
-//         {
-//           type: "cardio",
-//           name: "Running",
-//           duration: 25,
-//           distance: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date().setDate(new Date().getDate()-6),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Bench Press",
-//           duration: 20,
-//           weight: 285,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date().setDate(new Date().getDate()-5),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Bench Press",
-//           duration: 20,
-//           weight: 300,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date(new Date().setDate(new Date().getDate() - 4)),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Quad Press",
-//           duration: 30,
-//           weight: 300,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date(new Date().setDate(new Date().getDate() - 3)),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Bench Press",
-//           duration: 20,
-//           weight: 300,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     },
-//     {
-//       day: new Date(new Date().setDate(new Date().getDate() - 2)),
-//       exercises: [
-//         {
-//           type: "resistance",
-//           name: "Military Press",
-//           duration: 20,
-//           weight: 300,
-//           reps: 10,
-//           sets: 4
-//         }
-//       ]
-//     }
-//   ];
-
-//   Workout.deleteMany({})
-//     .then(() => Workout.collection.insertMany(workoutSeed))
-//     .then(data => {
-//       console.log(data.result.n + " records inserted!");
-//       process.exit(0);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       process.exit(1);
-//     });
-
 // Routes:
 
 module.exports = (app) => {
 
+    // Route to return all workouts and compute the total duration for each workout
     app.get("/api/workouts", (req, res) => {
-        // First sum up the duration of exercises for each document (day)
-        console.log("Adding a field");
+        // First sum up the duration of exercises for each workout (day)
         Workout.aggregate([
             {
                 $addFields: {
@@ -145,13 +14,63 @@ module.exports = (app) => {
                 }
             }
         ])
-        // Now return the database 
+            // Now return the database 
             .then(dbWorkouts => {
                 console.log("Workouts: " + dbWorkouts);
                 res.json(dbWorkouts);
             })
             .catch(err => {
-                res.status(400).json(err);
+                res.json(err);
+            });
+    });
+
+    // Route to return the last 7 workouts and compute the total duration for each workout
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.aggregate([
+            {
+                // Select the last 7 workouts
+                $limit: 7
+            },
+            {
+                // Now compute the total duration with these last 7 workouts
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" },
+                }
+            }
+        ])
+            // Now return the database 
+            .then(dbWorkouts => {
+                console.log("Workouts: " + dbWorkouts);
+                res.json(dbWorkouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    // Route to add a workout
+    app.post("/api/workouts", ({body}, res) => {
+        // set the day
+        let day = new Date();
+        // Create the workout
+        Workout.create({day, body})
+            .then(dbWorkouts => {
+                res.json(dbWorkouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    // Route to add exercises to an existing workout
+    app.put("/api/workouts/:id", (req, res) => {
+        let bodyExercises = req.body;
+        Workout.updateOne({ _id: req.params.id }, { $push: { "exercises": bodyExercises }})
+            .then(dbWorkouts => {
+                res.json(dbWorkouts);
+            })
+            .catch(err => {
+                res.json(err);
             });
     });
 };
